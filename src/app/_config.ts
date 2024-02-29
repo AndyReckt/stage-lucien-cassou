feb4cad1a889c810e5fa0227fb5f2d5286095264
Andy <alex123aze@gmail.com>
2024-02-29 17:06:59 +0100

    changement des images

--- src/app/_config.ts
+++ src/app/_config.ts
@@ -53,15 +53,15 @@ export const description = {
 
 export const projets: ImageData[] = [
     {
-        "id": 1,
-        "name": "Sample 1",
-        "url": "https://i.pinimg.com/originals/eb/f0/02/ebf002d6348c3ae432649da4418fce40.jpg",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sed suscipit sem. Mauris interdum sem elit, ut tempor quam interdum vel. Vivamus iaculis molestie ligula sit amet posuere. Mauris sit amet consequat quam. In nec venenatis dolor. Donec sed nibh ante. Pellentesque semper tortor vitae leo interdum faucibus. Cras in urna justo. In augue leo, vestibulum a varius sed, sollicitudin sed ante. Vivamus et sapien in orci pharetra lacinia id a elit. "
+        id: 1,
+        name: "Kalypso",
+        url: "/photo4.jpg",
+        description: "\"Kalypso\" est un spectacle de la compagnie Zfin Malta qui explore la mythologie grecque à travers la danse contemporaine. La performance se fonde sur le patrimoine préhistorique de l'île de Malte, la création des costumes et des masque s'inspire des différents travaux de recherche archéologique de l'archipel. Avec une chorégraphie inventive de Jacob Piccinino et une musique envoûtante, \"Kalypso\" transporte les spectateurs dans un voyage à travers les récits intemporels de la mythologie, offrant une interprétation unique et vibrante des histoires classiques."
     },
     {
-        "id": 2,
-        "name": "Sample 2",
-        "url": "https://i.imgur.com/EJOjIMC.jpeg",
-        "description": "Suspendisse at ligula ultrices lacus cursus bibendum. Aenean tincidunt, massa vel commodo mollis, lacus nibh imperdiet nulla, non sollicitudin metus purus sit amet lorem. Vivamus eget viverra arcu. Morbi sem ligula, pretium eget mi quis, ultricies tristique urna. Mauris gravida cursus velit a ultricies. Nullam sagittis et velit at pellentesque. Cras non risus condimentum, malesuada lacus vitae, congue diam. Curabitur sed tempor felis. Suspendisse ultricies mattis turpis ut molestie. Fusce rutrum massa odio, a suscipit mi ullamcorper id. Aliquam vitae lectus quis tellus commodo blandit. In accumsan, risus et pharetra bibendum, turpis magna venenatis metus, non viverra risus quam dignissim eros. Nulla tincidunt, sem non tempor ornare, nunc sapien auctor arcu, et feugiat massa nibh id risus. Fusce dignissim nisi in iaculis luctus. Morbi velit quam, imperdiet nec nulla sed, interdum tincidunt dolor. "
+        id: 2,
+        name: 'Monde parallèle',
+        url: "/photo7.jpg",
+        description: "Monde Parallèle est un spectacle qui mêle marionnettes et conte pour raconter l'histoire touchante de Lena et Max, deux sœurs d'une fratrie atypique. La marionnette de Lena a nécessité trois générations de prototypes pour parvenir à sa version finale. La forme finale est un mélange entre la marionnette sac, le masque larvaire et l'inspiration des réels traits des protagonistes. Ce conte marionnettique a caractère autobiographique explore les thèmes de la fratrie et du handicap, comment se tissent les liens d'amour quand le langage n'est pas possible ?"
     },
 ]
